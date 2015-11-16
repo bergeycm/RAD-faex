890df66e919ef6cb444c20762935b27635a588b9
Christina Bergey <christina.bergey@gmail.com>
2015-11-16 11:56:28 -0500

    Added scripts to downsample and rerun Make.
    Though the name is "downsample_bloods," sometimes fecal samples
    are downsampled.
    
    The second script creates fake precursor files for the downsampled BAMs,
    and calls Make to do variant inference.

---
+++ scripts/downsample_bloods.pl
@@ -0,0 +1,124 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+# ========================================================================================
+# --- Downsample blood reads to create blood BAMs that are the same size as fecal
+# ========================================================================================
+
+my @inds = ("Tx01", "Tx02", "Tx05", "Tx09", "Tx10");
+
+my $grep_prefix = "grep '^Mapped' ../NGS-map/reports/";
+my $grep_suffix = ".PE.bwa.baboon.aln_stats.passed.realn.txt | ";
+$grep_suffix   .= 'sed -e "s/Mapped reads: *\([0-9]*\)\t.*/\1/"';
+
+my $bam_prefix = "../NGS-map/results/";
+my $bam_suffix = ".PE.bwa.baboon.passed.realn.bam";
+
+foreach my $ind (@inds) {
+
+	print STDERR "PROCESSING $ind:\n";
+
+	my $awk_cmd = "awk -F \",\" '{ if (\$5 == \"" . $ind . "\") print \$0 }' ";
+	$awk_cmd .= "data/fecalRAD_individual_info.csv";
+	
+	my $sample_info = `$awk_cmd`;
+	
+	my @sample_info = split /\n/, $sample_info;
+	
+	my $blood_id;
+	my @fecal_ids;
+	my $blood_count;
+	my @fecal_count;
+	
+	my @blood_downsample_percents;
+	my @fecal_downsample_percents;
+	
+	foreach (@sample_info) {
+			
+		my @line_info = split /,/;
+				
+		if ($line_info[5] eq "blood") {
+		
+			$blood_id = $line_info[0];
+			
+			# Get count of mapped blood reads
+			my $grep_cmd = $grep_prefix . $blood_id . $grep_suffix;
+			
+			$blood_count = `$grep_cmd`;
+			
+			print STDERR "Blood reads: $blood_count\n";
+
+		} else {
+			my $this_fecal_id = $line_info[0];
+			push @fecal_ids, $this_fecal_id;
+			
+			# Get count of mapped fecal reads
+			my $grep_cmd = $grep_prefix . $this_fecal_id . $grep_suffix;
+
+			my $this_fecal_count = `$grep_cmd`;
+			chomp $this_fecal_count;
+			push @fecal_count, $this_fecal_count;
+			
+		}
+		
+	}
+	
+	print STDERR "Blood ID is $blood_id\n";
+	
+	my $blood_bam = $bam_prefix . $blood_id . $bam_suffix;
+	
+	for (my $i = 0; $i < scalar @fecal_ids; $i++) {
+
+		my $fecal_bam = $bam_prefix . $fecal_ids[$i] . $bam_suffix;
+	
+		my $fecal_perc = $fecal_count[$i] / $blood_count;
+	
+		print STDERR $fecal_ids[$i] . " has " . $fecal_count[$i] . " reads.\n";
+		print STDERR "\t" . $fecal_perc . " of the blood reads.\n";
+		
+		if ($fecal_perc < 1) {
+			# More blood reads
+			# Blood will be downsampled
+			$blood_downsample_percents[$i] = $fecal_perc;
+			$fecal_downsample_percents[$i] = 1;
+			
+			my $downsampled_blood_bam = $bam_prefix . $blood_id;
+			$downsampled_blood_bam .= "_samp-";
+			$downsampled_blood_bam .= lc (($fecal_ids[$i] =~ /fecalRAD-(.*)/)[0]);
+			$downsampled_blood_bam .= $bam_suffix;
+			
+			my $sam_cmd = "samtools view -s " . $fecal_perc;
+			$sam_cmd .= " -b " . $blood_bam;
+			$sam_cmd .= " > " . $downsampled_blood_bam;
+			
+			print STDERR "Command: [$sam_cmd]\n";
+			system($sam_cmd);
+			
+		} else {
+			# More fecal reads
+			# Feces will be downsampled
+			$blood_downsample_percents[$i] = 1;
+			$fecal_downsample_percents[$i] = 1 / $fecal_perc;
+			
+			my $downsampled_feces_bam = $bam_prefix . $fecal_ids[$i];
+			$downsampled_feces_bam .= "_samp-";
+			$downsampled_feces_bam .= lc (($blood_id =~ /fecalRAD-(.*)/)[0]);
+			$downsampled_feces_bam .= $bam_suffix;
+			
+			my $sam_cmd = "samtools view -s " . (1 / $fecal_perc);
+			$sam_cmd .= " -b " . $fecal_bam;
+			$sam_cmd .= " > " . $downsampled_feces_bam;
+			
+			print STDERR "Command: [$sam_cmd]\n";
+			system($sam_cmd);			
+		}
+	
+	}
+
+	print STDERR "=================================\n";
+	
+}
---
+++ scripts/prepare_to_process_downsampled.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------------------
+# --- Prepare and call make on downsampled BAMs
+# ----------------------------------------------------------------------------------------
+
+cd ../NGS-map
+
+module load samtools
+export SAMTOOLS=/share/apps/samtools/1.2/intel/bin
+
+for DS_BAM in results/*samp*passed.realn.bam; do
+
+    IND_ID=`echo $DS_BAM | sed -e "s/.*fecalRAD-/fecalRAD-/" -e "s/.PE.*//"`
+    IND_ID_W_PE_SE=${IND_ID}.PE
+
+    GENOME_NAME=baboon
+
+    # Make fake versions of precursor files
+    touch data/${IND_ID}.R1.fastq
+    touch data/${IND_ID}.R2.fastq
+    touch reports/${IND_ID}.read1.stats.zip
+    touch reports/${IND_ID}.read2.stats.zip
+    touch results/${IND_ID}.read1.bwa.${GENOME_NAME}.sai
+    touch results/${IND_ID}.PE.bwa.${GENOME_NAME}.sam
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.sam.bam
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.sam.bam.sorted.bam.bai
+    touch reports/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.aln_stats.txt
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.fixed.bam
+    touch reports/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.aln_stats.pairsfix.txt
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.fixed.filtered.bam.bai
+    touch reports/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.aln_stats.pairsfix.flt.txt
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.fixed.filtered.postdup.bam.bai 
+    touch reports/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.aln_stats.pairsfix.flt.postdup.txt
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.fixed.filtered.RG.bam
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.passed.bam.bai
+    touch reports/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.aln_stats.passed.txt
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.passed.bam.list
+
+    # Update last modified time of downsampled BAM
+    touch results/${IND_ID_W_PE_SE}.bwa.${GENOME_NAME}.passed.realn.bam
+    
+    # Index downsampled BAM
+    scripts/index_bam.sh results/${IND_ID}.PE.bwa.${GENOME_NAME}.passed.realn.bam
+    
+    echo $IND_ID >> data/individual_list.txt
+
+done
+
+# Submit jobs to call make
+### make -s -f full_analysis.mk indiv IND_ID=fecalRAD-BC3-BC7_samp-bc9-bc2
