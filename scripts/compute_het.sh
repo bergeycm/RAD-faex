fd570d7a4282d8586e5ad523e3dbabf3d2f0ae4a
Christina Bergey <christina.bergey@gmail.com>
2015-11-23 14:52:41 -0500

    Tweaked heterozygosity analysis, plotting.
    Added plot of observed homozygosity in addition to that of
    inbreeding coefficient, F.

--- scripts/compute_het.sh
+++ scripts/compute_het.sh
@@ -7,13 +7,27 @@
 module load vcftools
 
 # Fix issue with VCF not being fully diploid (. used instead of ./.)
-gunzip -c ../NGS-map/baboon.INDIV.pass.snp.vcf.gz | \
+gunzip -c ../NGS-map/baboon_snps_indiv/baboon.INDIV.pass.snp.vcf.gz | \
     sed -e "s:\t\.:\t./.:g" | \
     sed -e "s:\./\.:.:" | \
-    gzip -c > ../NGS-map/baboon.INDIV_DIPLOID.pass.snp.vcf.gz
+    gzip -c > ../NGS-map/baboon_snps_indiv/baboon.INDIV_DIPLOID.pass.snp.vcf.gz
 
-# Computer heterozygosity
-vcftools --gzvcf ../NGS-map/baboon.INDIV_DIPLOID.pass.snp.vcf.gz --het \
-    --out results/baboon.INDIV_DIPLOID.pass.snp
+# Compute heterozygosity
+vcftools --gzvcf ../NGS-map/baboon_snps_indiv/baboon.INDIV_DIPLOID.pass.snp.vcf.gz \
+    --het --out results/baboon.INDIV_DIPLOID.pass.snp
+
+# And HWE stats on a per SNP basis, including counts of heterozygotes and homozygotes
+vcftools --gzvcf ../NGS-map/baboon_snps_indiv/baboon.INDIV_DIPLOID.pass.snp.vcf.gz \
+    --hardy --out results/baboon.pass.snp
+
+# ----------------------------------------------------------------------------------------
+
+# Compute heterozygosity for multi-sample called SNPs
+vcftools --gzvcf ../NGS-map/baboon_snps_multi/baboon.pass.snp.vcf.gz \
+    --het --out results/baboon.pass.snp
+
+# And HWE stats on a per SNP basis, including counts of heterozygotes and homozygotes
+vcftools --gzvcf ../NGS-map/baboon_snps_multi/baboon.pass.snp.vcf.gz \
+    --hardy --out results/baboon.pass.snp
 
 exit
--- scripts/parse_heterozygosity.R
+++ scripts/parse_heterozygosity.R
@@ -5,8 +5,17 @@
 # ----------------------------------------------------------------------------------------
 
 library(stringr)
+library(ggplot2)
 
-het = read.table("results/baboon.INDIV_DIPLOID.pass.snp.het", header=TRUE)
+# Get heterozygosity file (vcftools output) from user
+args = commandArgs(trailingOnly = TRUE)
+het.file = args[1]	# e.g. "results/baboon.pass.snp.het"
+
+out.pdf.f       = paste0(het.file, ".pdf")
+out.pdf.hom_obs = paste0(het.file, ".obs_hom.pdf")
+out.txt = paste0(het.file, ".wilcox.txt")
+
+het = read.table(het.file, header=TRUE)
 
 ind.info = read.csv("data/fecalRAD_individual_info.csv")
 
@@ -14,22 +23,47 @@ het$NGS.ID = het$INDV
 het$NGS.ID = gsub("_samp.*", "", het$NGS.ID)
 het$NGS.ID = gsub(".PE", "", het$NGS.ID)
 
-ggplot(het.info, aes(Sample.type, F)) + geom_boxplot()
+het.info = merge(ind.info, het, by="NGS.ID")
+
+# Skipping. Very coarse boxplot of inbreeding coefficient, separated by sample type
+# ggplot(het.info, aes(Sample.type, F)) + geom_boxplot()
 
 het.info$ds = c("Raw", "Downsampled")[grepl("samp", het.info$INDV) + 1]
 het.info$is.special = grepl("(H2|HH)", het.info$Sample.ID)
 
 het.info$ds.pair = paste0("fecalRAD-", 
-							str_to_upper(gsub(".*_samp-", "", het.info$INDV)), 
-							".PE")
+							str_to_upper(gsub(".*_samp-", "", het.info$INDV)))
 het.info[which(het.info$ds == "Raw"),]$ds.pair = NA
 het.info$is.ds.pair = het.info$INDV %in% het.info$ds.pair
 
 het.info.pairs = subset(het.info, (het.info$ds == "Downsampled" | het.info$is.ds.pair) & 
 	het.info$is.special == FALSE)
 
-p = ggplot(het.info.pairs, aes(Sample.type, F)) + 
+p = ggplot(het.info.pairs, aes(Sample.type, O.HOM.)) + 
 	geom_boxplot() + 
-	geom_point(aes(color=Individual.ID, size=N_SITES), pch=1)
+	geom_point(aes(color=Individual.ID, size=N_SITES), pch=1) +
+	xlab("Sample type") + 
+	ylab("Observed Homozygous Sites") + 
+	scale_color_discrete(name="Individual",
+		breaks=c("Tx01", "Tx02", "Tx05", "Tx09", "Tx10"),
+		labels=c('SNPRC-13245','SNPRC-14068','SNPRC-25567','SNPRC-27958','SNPRC-28064')) + 
+	scale_size_continuous(name="Num. sites")
 
-ggsave("results/heterozygosity.pdf", plot=p)
+q = ggplot(het.info.pairs, aes(Sample.type, F)) + 
+	geom_boxplot() + 
+	geom_point(aes(color=Individual.ID, size=N_SITES), pch=1) +
+	xlab("Sample type") + 
+	ylab("F") + 
+	scale_color_discrete(name="Individual",
+		breaks=c("Tx01", "Tx02", "Tx05", "Tx09", "Tx10"),
+		labels=c('SNPRC-13245','SNPRC-14068','SNPRC-25567','SNPRC-27958','SNPRC-28064')) + 
+	scale_size_continuous(name="Num. sites")
+
+ggsave(out.pdf.hom_obs, plot=p)
+ggsave(out.pdf.f,       plot=q)
+
+# Test to see if F differs
+sink(out.txt)
+	wilcox.test(het.info.pairs[het.info.pairs$Sample.type == "feces",]$F, 
+		het.info.pairs[het.info.pairs$Sample.type == "blood",]$F)
+sink()
